82669a4a9be8cc7d2192403386de151d73457a4a
Maguette545 <maguettethiao23@gmail.com>
2025-04-30 16:05:24 +0000

    Second commit

--- Server.js
+++ Server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 5050;
- 
-app.use(express.static(path.join(__dirname, 'public')));
 
+ //route pour le html
+app.use(express.static(path.join(__dirname, 'views')));
+//route pour le css statique
+app.use(express.static(path.join(__dirname, 'public')));
 const now = new Date();
 const days = now.getDay();
 const time = now.getHours();
@@ -12,6 +14,7 @@ const time = now.getHours();
 const isDays = days >= 1 && days <= 5;
 const isTime = time >= 9 && time <= 17;
 
+//le middleware
 const midd = (req, res, next) => {
 if (isDays && isTime) {
    next();
@@ -19,16 +22,19 @@ if (isDays && isTime) {
    res.sendFile(path.join(__dirname, "views", "Closed.html"));
 }
 }
-app.get('/' , midd, (req,res) => {
+//route pour la page principale
+app.get('/', midd, (req,res) => {
    res.sendFile(path.join(__dirname, "views", "Accueil.html"))
 })
-app.get('/' , midd, (req,res) => {
+//route pour la page Contact
+app.get('/Contact', midd, (req,res) => {
    res.sendFile(path.join(__dirname, "views", "Contact.html"))
 })
-app.get('/' , midd, (req,res) => {
+//route pour la page Service
+app.get('/Service', midd, (req,res) => {
    res.sendFile(path.join(__dirname, "views", "Service.html"))
 })
-
+//Lancement du server
  app.listen(port , () => {
     console.log("Server is running on port :" , port);
     
